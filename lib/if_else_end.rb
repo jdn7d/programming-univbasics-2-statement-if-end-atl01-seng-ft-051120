8e74d6d809cfe7379e525cdc6a765294cdeac424
Jessie Donovan <donovanajessie@gmail.com>
2020-04-14 02:14:12 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,8 @@
-# Write your solution here
+current_time = Time.now
+current_time = current_time.to_i
+puts "Code before if...end"
+if current_time.to_i % 2 == 0
+   puts "Even!"
+ else
+   puts "Odd!"
+ end 
