8a4b88f296040b5cf67b8d1fbc1c50e979c9ce4a
Jessie Donovan <donovanajessie@gmail.com>
2020-04-14 02:12:43 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,8 @@
+current_time = Time.now
 current_time = current_time.to_i
 puts "Code before if...end"
-if current_time % 2 == 0
-   puts "
+if current_time.to_i % 2 == 0
+   puts "Even!"
  else
-   puts 
+   puts "Odd!"
  end 
